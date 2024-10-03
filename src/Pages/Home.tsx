5420df6dbdd379ce2dc0f8e403c1e49b601680b7
Jeremias Adriano <109630558+jeremiasadriano@users.noreply.github.com>
2024-10-03 18:55:40 +0200

    feat: xorturl ui

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,11 @@
+import Main from "../components/main/Main";
+import NavBar from "../components/navbar/NavBar";
+
+export default function Home() {
+  return (
+    <div className="">
+      <NavBar />
+      <Main />
+    </div>
+  );
+}
---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,8 @@
+export default function NavBar() {
+  return (
+    <div className="mt-3 flex justify-around text-2xl font-bold">
+      <div className="logo">XortUrl</div>
+      <div className="code">Github</div>
+    </div>
+  );
+}
